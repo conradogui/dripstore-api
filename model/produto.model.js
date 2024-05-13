3bb9d359dda57660ccdbc0a0b4c4d4c5be6bc7cd
conradogui <gui14fbce@gmail.com>
2024-05-13 20:13:58 -0300

    configurando banco e testando postman

---
+++ model/produto.model.js
@@ -0,0 +1,39 @@
+import { DataTypes } from "sequelize";
+import { sequelize } from "../db/database.js";
+
+export const Produto = sequelize.define("produtos", {
+  nome: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  descricao: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  desconto: {
+    type: DataTypes.DECIMAL,
+    allowNull: false,
+  },
+  preco: {
+    type: DataTypes.NUMBER,
+    allowNull: false,
+  },
+  ativo: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    defaultValue: true,
+  },
+  categoria: {
+    type: DataTypes.DECIMAL,
+    allowNull: false,
+  },
+  data_cadastro: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: new Date(),
+  },
+}, {
+  timestamps: true,
+  createdAt: 'data_cadastro',
+  updatedAt: false
+});
