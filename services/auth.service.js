0aeca65fc5369aacc70c76a86118e6c3ad162f94
conradogui <gui14fbce@gmail.com>
2024-06-24 21:53:59 -0300

    ajustando usuario

--- services/auth.service.js
+++ services/auth.service.js
@@ -1,38 +1,43 @@
-import pkg from "bcryptjs"
+import pkg from "bcryptjs";
 import db from "../model/index.js";
 import { Op } from "sequelize";
 
-const {hashSync, compareSync} = pkg
+const { hashSync, compareSync } = pkg;
 
 export const authService = {
-    signup: async (req, res) => {
-        const {nome, email, senha, perfis} = req.body
+  signup: async (req, res) => {
+    const { nome, email, senha, perfis } = req.body;
 
-        const usuario = await db.usuario.create({
-            nome,
-            email,
-            senha: hashSync(senha, 8)
-        })
+    const usuario = await db.usuario.create({
+      nome,
+      email,
+      senha: hashSync(senha, 8),
+    });
 
-        if(perfis.length > 0) {
-            const perfisDB = await db.perfil.findAll({
-                where: {
-                    nome: {
-                        [Op.in]: perfis
-                    }
-                }
-            })
+    if (perfis && perfis.length > 0) {
+      const perfisDB = await db.perfil.findAll({
+        where: {
+          nome: {
+            [Op.in]: perfis,
+          },
+        },
+      });
 
-            if(perfisDB) {
-                const usuarioPerfil = await usuario.addPerfils(perfisDB)
-                    res.status(201).json({
-                        msg: `Usuario ${usuario.nome} cadastrado com sucesso`,
-                        data: usuarioPerfil
-                    })
-                
-            }
-        }
-      },
+      if (perfisDB) {
+        const usuarioPerfil = await usuario.addPerfils(perfisDB);
+        res.status(201).json({
+          msg: `Usuario ${usuario.nome} cadastrado com sucesso`,
+          data: usuarioPerfil,
+        });
+      }
+    } else {
+      const usuarioPerfil = await usuario.addPerfils([1]);
+      res.status(201).json({
+        msg: `Usuario ${usuario.nome} cadastrado com sucesso`,
+        data: usuarioPerfil,
+      });
+    }
+  },
   signin: (req, res) => {},
   logout: (req, res) => {},
 };
