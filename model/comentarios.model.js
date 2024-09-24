eca205d6abd6cadff77e907853f5c503fb57c1fa
conradogui <gui14fbce@gmail.com>
2024-09-24 19:24:38 -0300

    começando ajuste de moderador

--- model/comentarios.model.js
+++ model/comentarios.model.js
@@ -2,43 +2,55 @@ import { DataTypes } from "sequelize";
 import { sequelize } from "../db/database.js";
 import { Produto } from "./produto.model.js";
 import { User } from "./user.model.js";
+import { Perfil } from "./perfil.model.js";
 
 export const Comentario = sequelize.define(
-    "comentario",
-    {
-      id: {
-        type: DataTypes.INTEGER, 
-        autoIncrement: true,
-        primaryKey: true,
-      },
-      texto: {
-        type: DataTypes.TEXT,
-        allowNull: false,
-      },
-      userId: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        references: {
-          model: "usuario", 
-          key: "id",
-        },
-      },
-      produtoId: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        references: {
-          model: "produtos", 
-          key: "id",
-        },
+  "comentario",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    texto: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "usuario",
+        key: "id",
       },
     },
-    {
-      timestamps: true,
-      tableName: "comentarios",
-    }
-  );
+    produtoId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "produtos", 
+        key: "id",
+      },
+    },
+    userType: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "perfil",
+        key: "id",
+      },
+    },
+  },
+  {
+    timestamps: true,
+    tableName: "comentarios",
+  }
+);
 
-Comentario.belongsTo(User, { foreignKey: 'userId' });
-Comentario.belongsTo(Produto, { foreignKey: 'produtoId' });
-Produto.hasMany(Comentario, { foreignKey: 'produtoId' });
-User.hasMany(Comentario, { foreignKey: 'userId' });
+// Definição das relações
+Comentario.belongsTo(User, { foreignKey: "userId" });
+Comentario.belongsTo(Produto, { foreignKey: "produtoId" });
+Comentario.belongsTo(Perfil, { foreignKey: "userType" });
+Produto.hasMany(Comentario, { foreignKey: "produtoId" });
+User.hasMany(Comentario, { foreignKey: "userId" });
+Perfil.hasMany(Comentario, { foreignKey: 'userType' });
